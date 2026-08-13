591ce360a1ce1fe065b9a0d2aa7a73ec5a359c0b
amorejesus <copetejesus699@gmail.com>
2026-08-13 08:39:33 -0500

    Bot.py pares normales

--- bot.py
+++ bot.py
@@ -38,9 +38,9 @@ CANDLE_COUNT = 60
 # ============================================================
 
 PAIRS = [
-    "EURUSD-OTC",
-    "GBPUSD-OTC",
-    "EURJPY-OTC",
+    "EURUSD",
+    "GBPUSD",
+    "EURJPY",
 ]
 
 
