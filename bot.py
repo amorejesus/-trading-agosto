75abfb82d21639fbe367a61000c53a06c7e7eeca
amorejesus <copetejesus699@gmail.com>
2026-08-16 18:01:04 -0500

    Bot.py v 6empuntos

--- bot.py
+++ bot.py
@@ -1,8 +1,3 @@
-# ============================================================
-# bot.py
-# IQ OPTION - ESTRATEGIA M1 + 12 VELAS DE 5 SEGUNDOS
-# ============================================================
-
 import os
 import time
 import requests
@@ -13,7 +8,7 @@ import strategy
 
 
 PAIR = "EURUSD-OTC"
-AMOUNT = 55000
+AMOUNT = 3333
 EXPIRATION = 1
 
 TIMEFRAME_5S = 5
